99d3a3818bfad81c37f4d49ddadcd19f1cb06550
Nikki Harris <nikkiharris@Nikkis-MacBook.local>
2019-08-15 10:46:32 -0400

    testing

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Welcome to my webpage
+        </h1>
+        <h7>
+        I am soo bored
+        </h7>
+
+        <button color= 'green'>this is a button that goes nowhere</button>
+
+
+
+    </div>
+  );
+}
+
+export default App;
